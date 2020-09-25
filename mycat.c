9e7531d7ae41d9cc19fedcb9a9316dec3c9a3635
seryj09 <68564759+seryj09@users.noreply.github.com>
2020-09-25 11:54:59 +0300

    Add files via upload

--- mycat.c
+++ mycat.c
@@ -12,13 +12,13 @@ void Cat(const int fd) {
     char str[4096];
     int n = read(fd, str, 4096), p = 0;
     if (n < 0) {
-        perror("open failed");
+        perror("cat: Permission denied");
         exit(1);
     }
     while (n > 0) {
         p = write(1, str, n);
         if (p < 0) {
-            perror("open failed");
+            perror("cat: Permission denied");
             exit(1);
         }
         n -= p;
@@ -32,9 +32,14 @@ int main(int argc, char const *argv[]) {
     }
 
     for (int i = 1; i < argc; i++) {
-        int fd = open(argv[i], O_RDWR | O_EXCL);    
+        int fd;
+        if ((fd= open(argv[i], O_RDWR | O_EXCL)) < 0) {
+            perror("open failed");
+            exit(1);
+        }
         Cat(fd);
+        close(fd);
     }
-    
+
     return 0;
 }
