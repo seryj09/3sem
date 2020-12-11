7fc0b57c20b2e8959969b4a923832ea4eb9b8b12
seryj09 <68564759+seryj09@users.noreply.github.com>
2020-12-11 15:55:15 +0300

    Update zachet.c

--- zachet.c
+++ zachet.c
@@ -15,23 +15,23 @@
 
 int main(int argc, char *argv[]) {
     int fd[2];
-    if (pipe(fd) < 0) {
+    if (pipe2(fd, O_NONBLOCK) < 0) {
         perror("Can\'t create pipe\n");
         exit(1);
     }
     
-    // char *str;
-    // int count = 0;
-    // str = "a";
-    // while (write(fd[1], str, 1) > 0) {
-    //     count++;
-    // }
-    // printf("%d", count);
+    char *str;
+    int count = 0;
+    str = "a";
+    while (write(fd[1], str, 1) == 1) {
+        count++;
+    }
+    printf("%d", count);
 
 
-    struct stat buf = {0};
-    fstat(fd[1], &buf);
-    printf("%lld\n", buf.st_size);
+    // struct stat buf = {0};
+    // fstat(fd[1], &buf);
+    // printf("%lld\n", buf.st_size);
 
     return 0;
 }
