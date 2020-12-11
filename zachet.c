72037c7afc16ed82ab9a4460903841130d85a462
seryj09 <68564759+seryj09@users.noreply.github.com>
2020-12-11 14:30:04 +0300

    Add files via upload

---
+++ zachet.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/sem.h>
+//10. Напишите программу, экспериментально определяющую размер буфера,
+// выделяемого операционной системой для организации pipe.
+
+int main(int argc, char *argv[]) {
+    int fd[2];
+    if (pipe(fd) < 0) {
+        perror("Can\'t create pipe\n");
+        exit(1);
+    }
+    char *str;
+    int count = 0;
+    str = "a";
+    while (write(1, str, 1) > 0) {
+        count++;
+    }
+    write(1, &count, 4);
+
+    return 0;
+}
